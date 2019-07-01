d7b15b8783c7802e58c373af778c9d97f233811e
Mark Pereira <markpereira17@gmail.com>
2019-07-01 15:26:43 +1000

    adding bundle and auto-run command for react native

--- AliceX/ReactNative/AliceX-Bridging-Header.h
+++ AliceX/ReactNative/AliceX-Bridging-Header.h
@@ -1,5 +1,10 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
+#ifndef AliceX-Bridging-Header_h
+#define AliceX-Bridging-Header_h
 
 #import <React/RCTBridgeModule.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#endif
